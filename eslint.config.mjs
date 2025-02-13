0a44db55952af37902e4b28ee4ff19054da7a9db
RogelioKG <rogeliokg0813@gmail.com>
2025-02-13 22:40:32 +0800

    UPDATE

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,3 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from '@antfu/eslint-config'
 
-export default withNuxt([
-  {
-    files: ['**/*.{vue,js,mjs,cjs,ts}'],
-    ignores: ['.nuxt', '.output', 'node_modules', 'dist'],
-    rules: {
-      'semi': ['error', 'never'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2],
-      'no-console': 'warn',
-      'arrow-parens': ['error', 'as-needed'],
-      'space-before-function-paren': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-    },
-  },
-])
+export default antfu()
